5e008d45db5b49b60345087883376e9643a6ec7a
harryle <lehoangsonsg@gmail.com>
2024-09-07 09:07:28 +0930

    feat: add a method to validate ID uniqueness

--- src/miappe_packaging/schema.py
+++ src/miappe_packaging/schema.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from collections.abc import Sequence, Set
 from dataclasses import dataclass, field
 from pathlib import Path
@@ -9,7 +11,7 @@ from typing import (
 from rdflib import BNode, Graph, IdentifiedNode, Literal, URIRef
 from rdflib.namespace import RDF
 
-from src.miappe_packaging.exceptions import AnnotationError, SchemaError
+from src.miappe_packaging.exceptions import AnnotationError, IdError, SchemaError
 from src.miappe_packaging.utils import cached_class_property
 
 
@@ -30,10 +32,120 @@ class Schema(TypedDict):
     attrs: dict[str, FieldInfo]
 
 
+class Registry:
+    _instance = None
+    models: dict[URIRef, type["Base"]] = dict()
+    _graph: Graph = Graph()
+    ID_Pool: set[IdentifiedNode] = set()
+
+    def __new__(self, *args, **kwargs):
+        if not self._instance:
+            self._instance = super().__new__(self, *args, **kwargs)
+        return self._instance
+
+    def validate_id_unique(self, id: IdentifiedNode) -> bool:
+        if id in self.ID_Pool:
+            return False
+        self.ID_Pool.add(id)
+        return True
+
+    def _parse(self, path: str | Path) -> None:
+        """Parse rdf store from path
+
+        Args:
+            path (str | Path): path description
+        """
+        self._graph.parse(path)
+
+    def serialize(
+        self,
+        destination: str | Path,
+        format: TypingLiteral[
+            "json-ld", "turtle", "xml", "pretty-xml", "n3", "nt", "trix"
+        ] = "json-ld",
+    ) -> None:
+        """Write to file for persistency.
+
+        Before the underlying graph is serialised, all semantic objects are first added to the graph
+
+        Args:
+            destination (str | Path): file path
+            format (TypingLiteral[ &quot;json, optional): supported serialisation formats. Defaults to "json-ld".
+        """
+        self.add_all()
+        self._graph.serialize(destination=destination, format=format)
+
+    def register(self, model: type["Base"]) -> None:
+        """Register model class with registry. Model classes are searchable with registry
+        by their rdfs:Resource reference
+
+        Args:
+            model (type[Base]): model class
+        """
+        self.models[model.rdf_resource] = model
+
+    def load(self, path: str | Path) -> None:
+        """Open file from path and create semantic class objects based on linked data.
+
+        Args:
+            path (str | Path): path to linked data file
+        """
+        self._parse(path)
+        for class_ref, class_model in self.models.items():
+            class_ids = self._graph.subjects(RDF.type, class_ref, unique=True)
+            for class_id in class_ids:
+                stmts = self._graph.predicate_objects(class_id, unique=True)
+                class_model.from_stmts(class_id, stmts)
+
+    def add(self, instance: "Base") -> None:
+        """Add an instance of a semantic class to current graph
+
+        Args:
+            instance (Base): semantic class instance
+        """
+
+        def atomic_add(name: str, info: FieldInfo, value: Any) -> None:
+            if issubclass(type(value), (Sequence, Set)) and not isinstance(value, str):
+                for item in value:
+                    atomic_add(name, info, item)
+            else:
+                datatype = info.get("range", None)
+                datatype = datatype if isinstance(datatype, URIRef) else None
+                literal_value = Literal(value, datatype=datatype)
+                try:
+                    self._graph.add((instance.ID, info["ref"], literal_value))
+                except KeyError:
+                    raise SchemaError(
+                        f"Missing required ref for schema field {name} of of class: {instance.__class__.__name__}"
+                    )
+
+        self._graph.add((instance.ID, RDF.type, instance.rdf_resource))
+        for name, info in instance.field_schema.items():
+            value = getattr(instance, name)
+            if value is not None:
+                atomic_add(name, info, value)
+
+    def add_all(self) -> None:
+        """Automatically add all semantic object instance to
+        graph to prepare to write to disk
+        """
+        for _, class_model in self.models.items():
+            for _, model_instance in class_model.store.items():
+                self.add(model_instance)
+
+
 @dataclass(kw_only=True)
 class Base:
+    """Semantic base class.
+
+    Semantic classes keep track of its instances by ID.
+    """
+
     __schema__: ClassVar[Schema]
+    """Schema object that maps the class attributes to semantic properties"""
     store: ClassVar[dict[str, "Base"]] = dict()
+    """Mapping between ID and class instance for tracking object instance"""
+    registry: ClassVar[Registry] = Registry()
 
     id: str | IdentifiedNode = field(default_factory=BNode)
 
@@ -46,11 +158,16 @@ class Base:
             raise
 
         # Register class at registry
-        Registry().register(cls)
+        cls.registry.register(cls)
         return super().__init_subclass__()
 
     def __post_init__(self) -> None:
-        self.store[self.ID] = self
+        if self.registry.validate_id_unique(self.ID):
+            self.store[self.ID] = self
+        else:
+            raise IdError(
+                f"Current ID already in use. Select a different ID to avoid clashing. ID value: {self.ID}"
+            )
 
     @classmethod
     def _ensure_matching_field_annotation(cls) -> None:
@@ -120,98 +237,3 @@ class Base:
             IdentifiedNode: id of current object.
         """
         return self.id if isinstance(self.id, IdentifiedNode) else URIRef(self.id)
-
-
-class Registry:
-    _instance = None
-    models: dict[URIRef, type[Base]] = dict()
-    _graph: Graph = Graph()
-
-    def __new__(self, *args, **kwargs):
-        if not self._instance:
-            self._instance = super().__new__(self, *args, **kwargs)
-        return self._instance
-
-    def _parse(self, path: str | Path) -> None:
-        """Parse rdf store from path
-
-        Args:
-            path (str | Path): path description
-        """
-        self._graph.parse(path)
-
-    def serialize(
-        self,
-        destination: str | Path,
-        format: TypingLiteral[
-            "json-ld", "turtle", "xml", "pretty-xml", "n3", "nt", "trix"
-        ] = "json-ld",
-    ) -> None:
-        """Write to file for persistency.
-
-        Before the underlying graph is serialised, all semantic objects are first added to the graph
-
-        Args:
-            destination (str | Path): file path
-            format (TypingLiteral[ &quot;json, optional): supported serialisation formats. Defaults to "json-ld".
-        """
-        self.add_all()
-        self._graph.serialize(destination=destination, format=format)
-
-    def register(self, model: type[Base]) -> None:
-        """Register model class with registry. Model classes are searchable with registry
-        by their rdfs:Resource reference
-
-        Args:
-            model (type[Base]): model class
-        """
-        self.models[model.rdf_resource] = model
-
-    def load(self, path: str | Path) -> None:
-        """Open file from path and create semantic class objects based on linked data.
-
-        Args:
-            path (str | Path): path to linked data file
-        """
-        self._parse(path)
-        for class_ref, class_model in self.models.items():
-            class_ids = self._graph.subjects(RDF.type, class_ref, unique=True)
-            for class_id in class_ids:
-                stmts = self._graph.predicate_objects(class_id, unique=True)
-                class_model.from_stmts(class_id, stmts)
-
-    def add(self, instance: Base) -> None:
-        """Add an instance of a semantic class to current graph
-
-        Args:
-            instance (Base): semantic class instance
-        """
-
-        def atomic_add(name: str, info: FieldInfo, value: Any) -> None:
-            if issubclass(type(value), (Sequence, Set)) and not isinstance(value, str):
-                for item in value:
-                    atomic_add(name, info, item)
-            else:
-                datatype = info.get("range", None)
-                datatype = datatype if isinstance(datatype, URIRef) else None
-                literal_value = Literal(value, datatype=datatype)
-                try:
-                    self._graph.add((instance.ID, info["ref"], literal_value))
-                except KeyError:
-                    raise SchemaError(
-                        f"Missing required ref for schema field {name} of of class: {instance.__class__.__name__}"
-                    )
-
-        self._graph.add((instance.ID, RDF.type, instance.rdf_resource))
-        for name, info in instance.field_schema.items():
-            value = getattr(instance, name)
-            if value is not None:
-                atomic_add(name, info, value)
-
-    def add_all(self) -> None:
-        """Automatically add all semantic object instance to
-        graph to prepare to write to disk
-        """
-        for _, class_model in self.models.items():
-            for _, model_instance in class_model.store.items():
-                self.add(model_instance)
